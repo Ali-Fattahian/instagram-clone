5ab1fb47d67a19ee7fdb82211321d7d112c0882f
Ali-Fattahian <ali.fattahian1378@gmail.com>
2021-12-21 00:58:28 +0330

    create tests for post and comment models and then create them

--- instagram_clone/core/models.py
+++ instagram_clone/core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from users.models import Profile
 
-# Create your models here.
+
+class Post(models.Model):
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='post')
+    image = models.ImageField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return f'Post id:{self.id} | author:{self.profile.username}'
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return f'Comment author: {self.profile.username} | on post: {self.post.id} by: {self.post.profile.username}'
---
+++ instagram_clone/core/tests/test_models.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from core.models import Post, Comment
+
+
+class TestPostModel(TestCase):
+
+    def setUp(self):
+        self.username = 'test_user'
+        self.email = 'test_user@gmail.com'
+        self.password = 'testpassword'
+        self.first_name = 'first test'
+        self.last_name = 'last test'
+        self.test_user = get_user_model().objects.create_user(
+            username=self.username, password=self.password, email=self.email, first_name=self.first_name, last_name=self.last_name,)
+        self.test_profile = self.test_user.profile
+        self.test_post = Post.objects.create(content='random string', profile=self.test_profile,
+                                             image='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg')
+
+    def test_create_post(self):
+        self.assertIsInstance(self.test_post, Post)
+
+
+class TestCommentModel(TestCase):
+    def setUp(self):
+        self.username = 'test_user'
+        self.email = 'test_user@gmail.com'
+        self.password = 'testpassword'
+        self.first_name = 'first test'
+        self.last_name = 'last test'
+        self.test_user = get_user_model().objects.create_user(
+            username=self.username, password=self.password, email=self.email, first_name=self.first_name, last_name=self.last_name,)
+        self.test_profile = self.test_user.profile
+        self.test_post = Post.objects.create(content='random string', profile=self.test_profile,
+                                             image='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg')
+        self.test_comment = Comment.objects.create(
+            content='random string', profile=self.test_profile, post=self.test_post)
+
+    def test_create_comment(self):
+        self.assertIsInstance(self.test_comment, Comment)
+
+    def test_comment_field(self):
+        self.assertEqual(self.test_comment.profile, self.test_profile)
+        self.assertEqual(self.test_comment.post, self.test_post)
+        self.assertEqual(self.test_comment.content, 'random string')
