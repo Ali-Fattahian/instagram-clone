e09bfad2c94b4672adc471b73e4f10c2bfb20ab3
Ali-Fattahian <ali.fattahian1378@gmail.com>
2021-12-21 15:28:03 +0330

    solve test_create_like_post by making a LikePost model

--- instagram_clone/core/tests/test_models.py
+++ instagram_clone/core/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from core.models import Post, Comment
+from core.models import Post, Comment, LikePost
 
 
 class TestPostModel(TestCase):
