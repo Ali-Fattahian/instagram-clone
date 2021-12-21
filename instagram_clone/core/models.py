fd2976f52c4afee65ae07025ec523adc6cdd24b6
Ali-Fattahian <ali.fattahian1378@gmail.com>
2021-12-21 15:39:08 +0330

    create LikeComment model and solve test_create_like_comment

--- instagram_clone/core/models.py
+++ instagram_clone/core/models.py
@@ -32,3 +32,13 @@ class LikePost(models.Model):
 
     def __str__(self):
         return f'profile {self.profile.username} liked post id {self.post.id} by {self.post.profile.username}'
+
+
+class LikeComment(models.Model):
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='profile_like_comment')
+    comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, related_name='comment_like')
+
+    def __str__(self):
+        return f'profile {self.profile.username} liked comment id {self.comment.id} by {self.comment.profile.username}'
