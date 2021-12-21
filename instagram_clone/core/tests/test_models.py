92c5ead0f9a23390ad9c7eb890422735f3cece5d
Ali-Fattahian <ali.fattahian1378@gmail.com>
2021-12-21 17:48:54 +0330

    make a test for creating a follow object

--- instagram_clone/core/tests/test_models.py
+++ instagram_clone/core/tests/test_models.py
@@ -76,7 +76,7 @@ class TestLikeComment(TestCase):
         self.first_name = 'first test'
         self.last_name = 'last test'
         self.test_user = get_user_model().objects.create_user(
-            username=self.username, password=self.password, email=self.email, first_name=self.first_name, last_name=self.last_name,)
+            username=self.username, password=self.password, email=self.email, first_name=self.first_name, last_name=self.last_name)
         self.test_profile = self.test_user.profile
         self.test_post = Post.objects.create(content='random string', profile=self.test_profile,
                                              image='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg')
@@ -88,3 +88,29 @@ class TestLikeComment(TestCase):
     def test_create_like_comment(self):
         """Test likecomment object created by profile and comment field given"""
         self.assertIsInstance(self.test_like_comment, LikeComment)
+
+
+class TestFollowModel(TestCase):
+    def setUp(self):
+        self.username1 = 'test_user'
+        self.email1 = 'test_user@gmail.com'
+        self.password1 = 'testpassword'
+        self.first_name1 = 'first test'
+        self.last_name1 = 'last test'
+        self.test_user1 = get_user_model().objects.create_user(
+            username=self.username1, password=self.password1, email=self.email1, first_name=self.first_name1, last_name=self.last_name1)
+
+        self.username2 = 'test_user2'
+        self.email2 = 'test_user@gmail.com2'
+        self.password2 = 'testpassword2'
+        self.first_name2 = 'first test2'
+        self.last_name2 = 'last test2'
+        self.test_user2 = get_user_model().objects.create_user(
+            username=self.username2, password=self.password2, email=self.email2, first_name=self.first_name2, last_name=self.last_name2)
+
+        self.test_follow = Follow.objects.create(
+            follower=self.test_user2, following=self.test_user1.id)
+
+    def test_create_follow(self):
+        """Test follow object created by follower and following field given """
+        self.assertIsInstance(self.test_follow, Follow)
