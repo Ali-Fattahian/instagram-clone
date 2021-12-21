bdcba82c2da2c726e2860fd543c84e2a94177b6b
Ali-Fattahian <ali.fattahian1378@gmail.com>
2021-12-21 15:15:00 +0330

    add description for each test in test_models of core blog and add TestCase for LikePost model

--- instagram_clone/core/tests/test_models.py
+++ instagram_clone/core/tests/test_models.py
@@ -18,6 +18,7 @@ class TestPostModel(TestCase):
                                              image='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg')
 
     def test_create_post(self):
+        """Test the creation of a post with content, profile and image given"""
         self.assertIsInstance(self.test_post, Post)
 
 
@@ -37,9 +38,29 @@ class TestCommentModel(TestCase):
             content='random string', profile=self.test_profile, post=self.test_post)
 
     def test_create_comment(self):
+        """Test the creation of a comment with content, profile and post given"""
         self.assertIsInstance(self.test_comment, Comment)
 
     def test_comment_field(self):
+        """Test if the value inside of each field equals the expected """
         self.assertEqual(self.test_comment.profile, self.test_profile)
         self.assertEqual(self.test_comment.post, self.test_post)
         self.assertEqual(self.test_comment.content, 'random string')
+
+
+class TestLikePostsModel(TestCase):
+    def setUp(self):
+        self.username = 'test_user'
+        self.email = 'test_user@gmail.com'
+        self.password = 'testpassword'
+        self.first_name = 'first test'
+        self.last_name = 'last test'
+        self.test_user = get_user_model().objects.create_user(
+            username=self.username, password=self.password, email=self.email, first_name=self.first_name, last_name=self.last_name,)
+        self.test_profile = self.test_user.profile
+        self.test_post = Post.objects.create(content='random string', profile=self.test_profile,
+                                             image='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg')
+        self.test_comment = Comment.objects.create(
+            content='random string', profile=self.test_profile, post=self.test_post)
+        self.test_like = LikePost.objects.create(
+            profile=self.test_profile, post=self.test_post)
