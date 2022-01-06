16b5eddcf61f55f6763062b3d786f057b1f77810
Ali-Fattahian <ali.fattahian1378@gmail.com>
2022-01-06 19:28:30 +0330

    add a test for not allowing user to like a post more than once

--- instagram_clone/core/tests/test_models.py
+++ instagram_clone/core/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
 from core.models import Post, Comment, LikePost, LikeComment
 
 
@@ -89,3 +90,32 @@ class TestLikeComment(TestCase):
         """Test likecomment object created by profile and comment field given"""
         self.assertIsInstance(self.test_like_comment, LikeComment)
 
+class TestLikePostModel(TestCase):
+    def setUp(self):
+        self.username = 'test_user'
+        self.email = 'test_user@gmail.com'
+        self.password = 'testpassword'
+        self.first_name = 'first test'
+        self.last_name = 'last test'
+        self.test_user = get_user_model().objects.create_user(
+            username=self.username, password=self.password, email=self.email, first_name=self.first_name, last_name=self.last_name)
+        self.test_profile = self.test_user.profile
+        self.test_post = Post.objects.create(content='random string', profile=self.test_profile,
+                                             image='post_images/demo-pic-1.jpg')
+
+        self.username2 = 'test_user2'
+        self.email2 = 'test_user2@gmail.com'
+        self.password2 = 'testpassword2'
+        self.first_name2 = 'first test2'
+        self.last_name2 = 'last test2'
+        self.test_user2 = get_user_model().objects.create_user(
+            username=self.username2, password=self.password2, email=self.email2, first_name=self.first_name2, last_name=self.last_name2)
+        self.test_profile2 = self.test_user2.profile
+
+    def test_cant_like_post_more_than_once(self):
+        """Test a user can't like another user's post more than once"""
+        LikePost.objects.create(
+            profile=self.test_profile2, post=self.test_post)
+        with self.assertRaises(IntegrityError, msg='Can\'t like the same post twice'):
+            LikePost.objects.create(
+                profile=self.test_profile2, post=self.test_post)
