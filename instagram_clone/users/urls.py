6285fbec91d2139852038c595da6117cc3fb89a0
Ali-Fattahian <ali.fattahian1378@gmail.com>
2022-02-01 01:55:35 +0330

    add profile-list page for search result

--- instagram_clone/users/urls.py
+++ instagram_clone/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,6 @@ urlpatterns = [
     path('log-in/', views.LogInView.as_view(), name='log-in'),
     path('edit-profile/<slug:slug>',
          views.EditProfileView.as_view(), name='edit-profile'),
+    path('search-results/',
+         TemplateView.as_view(template_name='users/profile-list.html'), name='search-results'),
 ]
