94c61b2a2770e0147146375e03d919572035be38
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-06-09 17:37:02 +0430

    remove the black background from suggested users picture on homepage and run collectstatic

--- instagram_clone/staticfiles/js/flash_messages.c52a682bd500.js
+++
@@ -1,5 +0,0 @@
-const flashMessage = document.querySelector('.alert');
-const messageCloseButton = document.querySelector('.alert__close');
-messageCloseButton.addEventListener('click', function () {
-  flashMessage.style.display = 'none';
-})
